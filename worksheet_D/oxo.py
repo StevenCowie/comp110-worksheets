f635cab37c18d8b18488edf92681714c08d56252
StevenCowie <steven.cowie@hotmail.co.uk>
2016-11-06 18:31:49 +0000

    Got program working

--- worksheet_D/oxo.py
+++ worksheet_D/oxo.py
@@ -1,29 +1,49 @@
+
 class OxoBoard:
     def __init__(self):
-        """ The initialiser. Initialise any fields you need here. """
-        raise NotImplementedError("TODO: implement __init__")
+        #Initialises board array
+        self.gameBoard = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
 
     def get_square(self, x, y):
-        """ Return 0, 1 or 2 depending on the contents of the specified square. """
-        raise NotImplementedError("TODO: implement get_square")
+        #Returns 0, 1 or 2 depending on the contents of the specified square.
+        return  self.gameBoard[x][y]
+
 
     def set_square(self, x, y, mark):
-        """ If the specified square is currently empty (0), fill it with mark and return True.
-            If the square is not empty, leave it as-is and return False. """
-        raise NotImplementedError("TODO: implement set_square")
+        #Checks content of square and fills empty spots with marks and returns True, is space is taken returns False
+        if self.gameBoard[x][y] == 0:
+            self.gameBoard[x][y] = mark
+            return True
+        else:
+            return False
+
 
     def is_board_full(self):
-        """ If there are still empty squares on the board, return False.
-            If there are no empty squares, return True. """
-        raise NotImplementedError("TODO: implement is_board_full")
+        #Checks every square to see if there are any empty squares left, returns True if none are empty
+        for x in xrange(3):
+            for y in xrange(3):
+                if self.gameBoard[x][y] == 0:
+                    return False
+        return True
+
 
     def get_winner(self):
-        """ If a player has three in a row, return 1 or 2 depending on which player.
-            Otherwise, return 0. """
-        raise NotImplementedError("TODO: implement get_winner")
+        # Checks whole board to see if there are 3 in row (vertical, horizontal and diagonal)
+        for i in xrange(3):
+            if self.gameBoard[i][0] == self.gameBoard[i][1] and self.gameBoard[i][2]!= 0:
+                return self.gameBoard[i][0]
+            elif self.gameBoard[0][i] == self.gameBoard[1][i] and self.gameBoard[2][i] != 0:
+                return self.gameBoard[0][i]
+            elif self.gameBoard[0][0] == self.gameBoard[1][1] and self.gameBoard[2][2] != 0:
+                return self.gameBoard[0][0]
+            elif self.gameBoard[0][2] == self.gameBoard[1][1] and self.gameBoard[2][0] != 0:
+                return self.gameBoard[0][2]
+        return 0
+
 
     def show(self):
-        """ Display the current board state in the terminal. You should not need to edit this. """
+        """ Display the current board state in the terminal. """
         for y in xrange(3):
             if y > 0:
                 print "--+---+--"
@@ -37,7 +57,7 @@ class OxoBoard:
 
 
 def input_square():
-    """ Prompt the player to enter a square. You should not need to edit this. """
+    """ Prompt the player to enter a square. """
     while True:
         input = raw_input("Enter x,y where x=0,1,2, y=0,1,2: ")
         if input.count(',') != 1:
